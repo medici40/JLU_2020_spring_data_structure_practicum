95c9fe90c62a43cc141a267336cf09f5b05889f5
Iris Meidici <48881811+medici40@users.noreply.github.com>
2020-04-16 18:53:08 +0800

    Update chess.cpp

--- chess.cpp
+++ chess.cpp
@@ -15,16 +15,6 @@
 #define EMPTY 2
 #define INF INT_MAX
 #define nonSenseInt -1
-// #define C6 1000000
-// #define A5 100000
-// #define S5 50000
-// //#define A4 50000
-// #define S4 10000
-// #define A3 10000
-// #define I3 5000
-// #define S3 1000
-// #define A2 100
-// #define S2 10
 #define re(i,a,b) for(int i=a;i<b;++i)
 #define max(a,b) a>b?a:b
 #define min(a,b) a<b?a:b
@@ -78,13 +68,6 @@ int compare8(int path[8]);
 void numberReturn(int simuBoard[19][19], int color, int CS[8]);
 int ifwin(int simuBoard[19][19], int computerside);
 int evaluate(int computerside, int myBoard[19][19]);//整体局面估分
-void ROW(int path[8], int sim[19][19], int m, int n, int color)
-{
-    for (int i = 0; i < 8; i++)
-    {
-        path[i] = sim[m][n + i];
-    }
-}
 void COL(int path[8], int sim[19][19], int m, int n, int color)
 {
     for (int i = 0; i < 8; i++)
@@ -126,6 +109,8 @@ int compare7(int path[7])
 }
 int compare8(int path[8])
 {
+    int num = 0;
+    int n = 0;
     int A5_2[8] = { 0,0,1,1,1,1,0,1 };
     int A5_3[8] = { 1,0,1,1,1,1,0,0 };
     int r = 0;
@@ -133,7 +118,6 @@ int compare8(int path[8])
     if (r == 1) return 1;
     r = equal(path, path + 8, A5_3);
     if (r == 1) return 1;
-
     int S5_1[8] = { -1,1,1,1,1,1,0 };
     int S5_2[8] = { 0,1,1,1,1,1,-1 };
     r = equal(path, path + 8, S5_1);
@@ -142,8 +126,8 @@ int compare8(int path[8])
     if (r == 1) return 2;
     if (path[0] == -1 || path[1] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 2; i < 8; i++)
         {
             if (path[i] == 1) num++;
@@ -153,8 +137,8 @@ int compare8(int path[8])
     }
     if (path[7] == -1 || path[6] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 5; i >= 0; i--)
         {
             if (path[i] == 1) num++;
@@ -163,16 +147,14 @@ int compare8(int path[8])
         if (num == 5 && n == 1) return 2;
     }
     //眠五检测完毕
-
     int A4[8] = { 0,0,1,1,1,1,0,0 };
     r = equal(path, path + 8, A4);
     if (r == 1) return 2;
     //活四检测完毕
-
     if (path[0] == -1 || path[7] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 1; i < 7; i++)
         {
             if (path[i] == 1) num++;
@@ -181,7 +163,6 @@ int compare8(int path[8])
         if (num == 4 && n == 2) return 3;
     }
     //眠四检测完成
-
     int A3_1[8] = { 0,0,1,1,1,0,0,0 };
     int A3_2[8] = { 0,0,1,1,0,1,0,0 };
     int A3_3[8] = { 0,0,1,0,1,1,0,0 };
@@ -195,13 +176,12 @@ int compare8(int path[8])
     r = equal(path, path + 8, A3_4);
     if (r == 1) return 3;
     //活三检测完毕
-
     if (path[0] == -1 || path[7] == -1)
     {
         if (path[1] == 0 && path[6] == 0)
         {
-            int num = 0;
-            int n = 0;
+            num = 0;
+            n = 0;
             for (int i = 2; i < 6; i++)
             {
                 if (path[i] == 1) num++;
@@ -211,11 +191,10 @@ int compare8(int path[8])
         }
     }
     //朦胧三判断完毕
-
     if (path[0] == -1 || path[2] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 2; i < 8; i++)
         {
             if (path[i] == 1) num++;
@@ -226,8 +205,8 @@ int compare8(int path[8])
 
     if (path[7] == -1 || path[6] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 5; i >= 0; i--)
         {
             if (path[i] == 1) num++;
@@ -248,8 +227,8 @@ int compare8(int path[8])
 
     if (path[0] == -1 || path[1] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 2; i < 8; i++)
         {
             if (path[i] == 1) num++;
@@ -259,8 +238,8 @@ int compare8(int path[8])
     }
     if (path[0] == -1 || path[7] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 2; i < 7; i++)
         {
             if (path[i] == 1) num++;
@@ -270,8 +249,8 @@ int compare8(int path[8])
     }
     if (path[7] == -1 || path[6] == -1)
     {
-        int num = 0;
-        int n = 0;
+        num = 0;
+        n = 0;
         for (int i = 0; i < 6; i++)
         {
             if (path[i] == 1) num++;
@@ -327,7 +306,6 @@ void numberReturn(int simuBoard[19][19], int color, int CS[8])
                 CS[r]++;
             }
         }
-
     }
     //斜向判断
     for (int i = 0; i < 11; i++)
